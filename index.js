e3ddba298d521d90ce7ca6c4798c67e70cc7a5c9
Giani Segatto <gsegatto@travix.com>
2019-05-25 19:02:37 +0100

    removed comments

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// const ContextLoader = require("leap-context").ContextLoader;
 const LeapApplication = require("leap-boot").LeapApplication;
-
-// const contextLoader = new ContextLoader();
-
-// contextLoader.load(process.cwd() + "/src/main/node/");
 
 LeapApplication.start();
